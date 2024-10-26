8fc92bb8ac561502a86ba2e094cfd80fc53ad514
Naimur Rahman Naim <56504158+naimekattor@users.noreply.github.com>
2024-10-27 01:30:20 +0600

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { ThemeProvider } from "./Context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
